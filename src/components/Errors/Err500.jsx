86618e02e867372efa1fdef3e58cba0cc2a6b023
Lev <levdanneman@gmail.com>
2021-02-01 20:45:59 +0200

    errors

--- src/components/Errors/Err500.jsx
+++ src/components/Errors/Err500.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import "./errors.css";
-import docimg2 from "./docimg2.png"
-
-
+import docimg2 from "./docimg2.png";
 
 function Err500() {
   return (
@@ -10,9 +8,11 @@ function Err500() {
       <div className="doctorImgDiv">
         <img className="doctorImg" src={docimg2} alt="Doctor IMG"></img>
       </div>
-      <div>
-        Our site does not seem to have a pulse right now. Resuscitation efforts
-        underway.
+      <div className="error500-message">
+        <p>
+          Our site does not seem to have a pulse right now. Resuscitation
+          efforts underway.
+        </p>
       </div>
     </div>
   );
