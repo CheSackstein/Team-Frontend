099b25ed78d584982534b676540b128726b818a5
BetoShiver <69962093+BetoShiver@users.noreply.github.com>
2021-02-02 14:28:50 +0200

    Home component migrated and cleaned

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,63 +1,11 @@
-import App from "../../App";
 import React, { useEffect, useState } from "react";
-import axios from "axios";
-import HomeImage from "./bOOKit.png";
 import {auth} from '../../lib/FetchShortcuts';
-import "./Home.css";
-import {
-  Button,
-  Modal,
-  ModalHeader,
-  ModalBody,
-  ModalFooter,
-  Form,
-  Input,
-  Label,
-  FormText,
-  Collapse,
-  Container,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink,
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem,
-  NavbarText,
-  FormGroup,
-} from "reactstrap";
-import { Link, useHistory } from "react-router-dom";
+import home from './Home.module.css'
 
-function Home(props) {
-  const [user, setUser] = useState("");
-  const [findUser, getUser] = useState("");
-  const [modalLog, setModalLog] = useState(false);
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [email, setEmail] = useState("Hi");
-  const [cell, setCell] = useState("");
-  const [password, setPassword] = useState("");
-  const [passwordConfirm, setPasswordConfirm] = useState("");
-  const toggleLog = () => setModalLog(!modalLog);
-  const [modalSign, setModalSign] = useState(false);
-  const history = useHistory();
+function Home() {
 
 
-  useEffect( async ()=>{
-    // check if logged 
-    auth();
-    // console.log('response: ', response);
-    // user = response.json()
-    // console.log('user: ', user);
-
-  },[])
-
-  return (
-    <div id="Home" className="Home">
-    </div>
-  );
+  return <div className={home.Home}>
+  </div>;
 }
 export default Home;
